f8a9671f9b57796ff020b1d29259dbeeaae8192e
Aman Dangol <amandangol423@gmail.com>
2023-12-12 22:28:36 +0545

    added local storage

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 let inputText = document.querySelector("input");
 let unorederedList = document.querySelector(".list");
 let id = 0;
+
 function addList() {
   if (inputText.value == "") {
     alert("empty string");
   } else {
     unorederedList.appendChild(createList());
+    store();
     inputText.value = "";
   }
 }
 function createList() {
   // creating an <li></li> element
   let li = document.createElement("li");
-//   adding a class name
-li.className="l"
+  //   adding a class name
+  li.className = "l";
   // adding the value we got from input
   li.innerHTML = `<span>${inputText.value}</span>`;
   //   creating and adding the a button to the text
@@ -36,21 +38,35 @@ inputText.addEventListener("keydown", keydetect);
 window.addEventListener("click", (e) => {
   if (e.target.className == "removeButton") {
     e.target.parentElement.remove();
+    store();
   }
   if (e.target.tagName == "LI") {
     console.log(e);
     e.target.children[0].style.textDecoration = "line-through";
+    store();
   }
-  if (e.target.tagName=="SPAN") {
-    e.target.style.textDecoration = underlinedOrNor(e.target.style.textDecoration);
+  if (e.target.tagName == "SPAN") {
+    e.target.style.textDecoration = underlinedOrNor(
+      e.target.style.textDecoration
+    );
+    store();
   }
 });
 
-function underlinedOrNor(decor){
-    if (decor=="") {
-        return "line-through"
-    }
-    else{
-       return "";
-    }
+function underlinedOrNor(decor) {
+  if (decor == "") {
+    return "line-through";
+  } else {
+    return "";
+  }
 }
+
+function store() {
+  console.log(localStorage.setItem("list", unorederedList.innerHTML));
+}
+
+function load() {
+  unorederedList.innerHTML = localStorage.getItem("list");
+}
+
+load();
