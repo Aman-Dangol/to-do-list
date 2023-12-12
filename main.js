8bb73d798f9aede9ddafa99124a2e2cd6073f8da
Aman Dangol <amandangol423@gmail.com>
2023-12-12 21:08:12 +0545

    only text underlined

--- main.js
+++ main.js
@@ -6,20 +6,18 @@ function addList() {
     alert("empty string");
   } else {
     unorederedList.appendChild(createList());
-    inputText.value= "";
-
+    inputText.value = "";
   }
 }
 function createList() {
   // creating an <li></li> element
   let li = document.createElement("li");
-  //   assigning an id;
-  li.id = id;
-  id++;
+//   adding a class name
+li.className="l"
   // adding the value we got from input
-  li.innerHTML = inputText.value;
+  li.innerHTML = `<span>${inputText.value}</span>`;
   //   creating and adding the a button to the text
-  li.appendChild(createButton(id));
+  li.appendChild(createButton());
 
   return li;
 }
@@ -28,9 +26,10 @@ function keydetect(e) {
     addList();
   }
 }
-function createButton(id) {
+function createButton() {
   let b = document.createElement("button");
   b.innerHTML = "remove";
+  b.value = "remove";
   b.className = "removeButton";
   return b;
 }
@@ -39,7 +38,12 @@ window.addEventListener("click", (e) => {
   if (e.target.className == "removeButton") {
     e.target.parentElement.remove();
   }
-  if (e.target.tagName == "LI") {
+  if (e.target.tagName.className== "LI") {
+    console.log(e);
+    e.target.children[0].style.textDecoration = "line-through";
+  }
+  if (e.target.tagName=="SPAN") {
     e.target.style.textDecoration = "line-through";
   }
 });
+
